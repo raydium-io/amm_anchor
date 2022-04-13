5855532fbc0e4e3f347bfa3898f1053b8c971d5b
wangzhen <wangzhen@wbfex.com>
2022-04-13 19:29:17 +0800

    implement amm anchor instructions

---
+++ src/accounts.rs
@@ -0,0 +1,307 @@
+//! Accounts structs for Raydium AMM / Liquidity.
+
+use anchor_lang::prelude::*;
+
+/// Accounts for an `pre_initialize` instruction.
+#[derive(Accounts, Clone)]
+pub struct PreInitialize<'info> {
+    /// AMM target orders account
+    #[account(mut)]
+    pub amm_target_orders: AccountInfo<'info>,
+    /// Withdraw queue Account. To save withdraw dest_coin & dest_pc account with must cancle orders.
+    #[account(mut)]
+    pub pool_withdraw_queue: AccountInfo<'info>,
+    /// Amm authority, a PDA account derived with seed `amm authority` and amm program address
+    pub amm_authority: AccountInfo<'info>,
+    /// Pool lp mint account. Must be empty, owned by $authority.
+    #[account(mut)]
+    pub lp_mint: AccountInfo<'info>,
+    /// Coin mint account
+    pub coin_mint: AccountInfo<'info>,
+    /// Pc mint account
+    pub pc_mint: AccountInfo<'info>,
+    /// Pool_token_coin Account. Must be non zero, owned by $authority
+    #[account(mut)]
+    pub pool_coin_token_account: AccountInfo<'info>,
+    /// Pool_token_pc Account. Must be non zero, owned by $authority.
+    #[account(mut)]
+    pub pool_pc_token_account: AccountInfo<'info>,
+    /// Token_temp_lp Account. To save withdraw lp with must cancle orders as temp to transfer later.
+    #[account(mut)]
+    pub pool_temp_lp_token_account: AccountInfo<'info>,
+    /// Serum market Account. serum_dex program is the owner.
+    pub serum_market: AccountInfo<'info>,
+    /// The user wallet create the pool
+    #[account(signer)]
+    pub user_wallet: AccountInfo<'info>,
+    #[account(address = spl_token::ID)]
+    pub spl_token_program: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+/// Accounts for an `initialize` instruction.
+#[derive(Accounts, Clone)]
+pub struct Initialize<'info> {
+    /// The new amm Account to create
+    #[account(mut)]
+    pub amm: AccountInfo<'info>,
+    /// Amm authority, a PDA account derived with seed `amm authority` and amm program address
+    pub amm_authority: AccountInfo<'info>,
+    /// Amm open_orders Account
+    #[account(mut)]
+    pub amm_open_orders: AccountInfo<'info>,
+    /// Pool lp mint account. Must be empty, owned by $authority.
+    #[account(mut)]
+    pub lp_mint: AccountInfo<'info>,
+    /// Coin mint account
+    pub coin_mint: AccountInfo<'info>,
+    /// Pc mint account
+    pub pc_mint: AccountInfo<'info>,
+    /// Pool_token_coin Account. Must be non zero, owned by $authority
+    pub pool_coin_token_account: AccountInfo<'info>,
+    /// Pool_token_pc Account. Must be non zero, owned by $authority.
+    pub pool_pc_token_account: AccountInfo<'info>,
+    /// Withdraw queue Account. To save withdraw dest_coin & dest_pc account with must cancle orders.
+    #[account(mut)]
+    pub pool_withdraw_queue: AccountInfo<'info>,
+    /// Pool target orders account
+    #[account(mut)]
+    pub pool_target_orders_account: AccountInfo<'info>,
+    /// Token_dest_lp Account. To deposit the initial pool token supply, user_wallet is the owner.
+    #[account(mut)]
+    pub pool_lp_token_account: AccountInfo<'info>,
+    /// Token_temp_lp Account. To save withdraw lp with must cancle orders as temp to transfer later.
+    pub pool_temp_lp_token_account: AccountInfo<'info>,
+    /// Serum dex program.
+    pub serum_program: AccountInfo<'info>,
+    /// Serum market Account. serum_dex program is the owner.
+    pub serum_market: AccountInfo<'info>,
+    /// The user wallet create the pool
+    #[account(signer)]
+    pub user_wallet: AccountInfo<'info>,
+    #[account(address = spl_token::ID)]
+    pub spl_token_program: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+/// Accounts for an `deposit` instruction.
+#[derive(Accounts, Clone)]
+pub struct Deposit<'info> {
+    /// Amm Account
+    #[account(mut)]
+    pub amm: AccountInfo<'info>,
+    /// Amm authority, a PDA account derived with seed `amm authority` and amm program address
+    pub amm_authority: AccountInfo<'info>,
+    /// AMM open_orders Account.
+    #[account(mut)]
+    pub amm_open_orders: AccountInfo<'info>,
+    /// AMM target orders account. To store plan orders infomations.
+    #[account(mut)]
+    pub amm_target_orders: AccountInfo<'info>,
+    /// LP mint account. Must be empty, owned by $authority.
+    #[account(mut)]
+    pub lp_mint: AccountInfo<'info>,
+    /// Pool_token_coin account, $authority can transfer amount.
+    #[account(mut)]
+    pub pool_coin_token_account: AccountInfo<'info>,
+    /// Pool_token_pc account, $authority can transfer amount.
+    #[account(mut)]
+    pub pool_pc_token_account: AccountInfo<'info>,
+    /// Serum market account, serum_dex program is the owner.
+    pub serum_market: AccountInfo<'info>,
+    /// User coin token account to deposit into.
+    #[account(mut)]
+    pub user_coin_token_account: AccountInfo<'info>,
+    /// User pc token account to deposit into.
+    #[account(mut)]
+    pub user_pc_token_account: AccountInfo<'info>,
+    /// User lp token account, to deposit the generated tokens, user is the owner
+    #[account(mut)]
+    pub user_lp_token_account: AccountInfo<'info>,
+    /// User wallet account
+    #[account(signer)]
+    pub user_owner: AccountInfo<'info>,
+    #[account(address = spl_token::ID)]
+    pub spl_token_program: AccountInfo<'info>,
+}
+
+/// Accounts for an `withdraw` instruction.
+#[derive(Accounts, Clone)]
+pub struct Withdraw<'info> {
+    /// Amm account
+    #[account(mut)]
+    pub amm: AccountInfo<'info>,
+    /// Amm authority Account
+    pub amm_authority: AccountInfo<'info>,
+    /// amm open_orders Account
+    #[account(mut)]
+    pub amm_open_orders: AccountInfo<'info>,
+    /// amm target_orders Account. To store plan orders infomations.
+    #[account(mut)]
+    pub amm_target_orders: AccountInfo<'info>,
+    /// pool lp mint account. Must be empty, owned by $authority.
+    #[account(mut)]
+    pub lp_mint: AccountInfo<'info>,
+    /// pool_token_coin Amm Account to withdraw FROM,
+    #[account(mut)]
+    pub pool_coin_token_account: AccountInfo<'info>,
+    /// pool_token_pc Amm Account to withdraw FROM,
+    #[account(mut)]
+    pub pool_pc_token_account: AccountInfo<'info>,
+    /// withdraw queue Account
+    #[account(mut)]
+    pub pool_withdraw_queue: AccountInfo<'info>,
+    /// token_temp_lp Account
+    #[account(mut)]
+    pub pool_temp_lp_token_account: AccountInfo<'info>,
+    /// serum dex program id
+    pub serum_program: AccountInfo<'info>,
+    /// serum market Account. serum_dex program is the owner.
+    #[account(mut)]
+    pub serum_market: AccountInfo<'info>,
+    /// coin_vault Account
+    #[account(mut)]
+    pub serum_coin_vault_account: AccountInfo<'info>,
+    /// pc_vault Account
+    #[account(mut)]
+    pub serum_pc_vault_account: AccountInfo<'info>,
+    /// vault_signer Account
+    pub serum_vault_signer: AccountInfo<'info>,
+    /// user lp token Account. Source lp, amount is transferable by $authority.
+    #[account(mut)]
+    pub user_lp_token_account: AccountInfo<'info>,
+    /// user token coin Account. user Account to credit.
+    #[account(mut)]
+    pub user_coin_token_account: AccountInfo<'info>,
+    /// user token pc Account. user Account to credit.
+    #[account(mut)]
+    pub user_pc_token_account: AccountInfo<'info>,
+    /// User wallet account
+    #[account(signer)]
+    pub user_owner: AccountInfo<'info>,
+    /// Serum event queue account
+    #[account(mut)]
+    pub serum_event_q: AccountInfo<'info>,
+    // Serum bid account
+    #[account(mut)]
+    pub serum_bids: AccountInfo<'info>,
+    // Serum ask account
+    #[account(mut)]
+    pub serum_asks: AccountInfo<'info>,
+    #[account(address = spl_token::ID)]
+    pub spl_token_program: AccountInfo<'info>,
+}
+
+/// Accounts for an `swap_base_in` instruction.
+#[derive(Accounts, Clone)]
+pub struct SwapBaseIn<'info> {
+    /// amm Account
+    #[account(mut)]
+    pub amm: AccountInfo<'info>,
+    /// Amm authority Account
+    pub amm_authority: AccountInfo<'info>,
+    /// amm open_orders Account
+    #[account(mut)]
+    pub amm_open_orders: AccountInfo<'info>,
+    /// amm target_orders Account
+    #[account(mut)]
+    pub amm_target_orders: AccountInfo<'info>,
+    /// pool_token_coin Amm Account to swap FROM or To,
+    #[account(mut)]
+    pub pool_coin_token_account: AccountInfo<'info>,
+    /// pool_token_pc Amm Account to swap FROM or To,
+    #[account(mut)]
+    pub pool_pc_token_account: AccountInfo<'info>,
+    /// serum dex program id
+    pub serum_program: AccountInfo<'info>,
+    /// serum market Account. serum_dex program is the owner.
+    #[account(mut)]
+    pub serum_market: AccountInfo<'info>,
+    /// bids Account
+    #[account(mut)]
+    pub serum_bids: AccountInfo<'info>,
+    /// asks Account
+    #[account(mut)]
+    pub serum_asks: AccountInfo<'info>,
+    /// event_q Account
+    #[account(mut)]
+    pub serum_event_queue: AccountInfo<'info>,
+    /// coin_vault Account
+    #[account(mut)]
+    pub serum_coin_vault_account: AccountInfo<'info>,
+    /// pc_vault Account
+    #[account(mut)]
+    pub serum_pc_vault_account: AccountInfo<'info>,
+    /// vault_signer Account
+    #[account(mut)]
+    pub serum_vault_signer: AccountInfo<'info>,
+    /// user source token Account. user Account to swap from.
+    #[account(mut)]
+    pub user_source_token_account: AccountInfo<'info>,
+    /// user destination token Account. user Account to swap to.
+    #[account(mut)]
+    pub user_destination_token_account: AccountInfo<'info>,
+    /// user owner Account
+    #[account(signer)]
+    pub user_source_owner: AccountInfo<'info>,
+    #[account(address = spl_token::ID)]
+    pub spl_token_program: AccountInfo<'info>,
+}
+
+/// Accounts for an `swap_base_out` instruction.
+#[derive(Accounts, Clone)]
+pub struct SwapBaseOut<'info> {
+    /// amm Account
+    #[account(mut)]
+    pub amm: AccountInfo<'info>,
+    /// Amm authority Account
+    pub amm_authority: AccountInfo<'info>,
+    /// amm open_orders Account
+    #[account(mut)]
+    pub amm_open_orders: AccountInfo<'info>,
+    /// amm target_orders Account
+    #[account(mut)]
+    pub amm_target_orders: AccountInfo<'info>,
+    /// pool_token_coin Amm Account to swap FROM or To,
+    #[account(mut)]
+    pub pool_coin_token_account: AccountInfo<'info>,
+    /// pool_token_pc Amm Account to swap FROM or To,
+    #[account(mut)]
+    pub pool_pc_token_account: AccountInfo<'info>,
+    /// serum dex program id
+    pub serum_program: AccountInfo<'info>,
+    /// serum market Account. serum_dex program is the owner.
+    #[account(mut)]
+    pub serum_market: AccountInfo<'info>,
+    /// bids Account
+    #[account(mut)]
+    pub serum_bids: AccountInfo<'info>,
+    /// asks Account
+    #[account(mut)]
+    pub serum_asks: AccountInfo<'info>,
+    /// event_q Account
+    #[account(mut)]
+    pub serum_event_queue: AccountInfo<'info>,
+    /// coin_vault Account
+    #[account(mut)]
+    pub serum_coin_vault_account: AccountInfo<'info>,
+    /// pc_vault Account
+    #[account(mut)]
+    pub serum_pc_vault_account: AccountInfo<'info>,
+    /// vault_signer Account
+    #[account(mut)]
+    pub serum_vault_signer: AccountInfo<'info>,
+    /// user source token Account. user Account to swap from.
+    #[account(mut)]
+    pub user_source_token_account: AccountInfo<'info>,
+    /// user destination token Account. user Account to swap to.
+    #[account(mut)]
+    pub user_destination_token_account: AccountInfo<'info>,
+    /// user owner Account
+    #[account(signer)]
+    pub user_source_owner: AccountInfo<'info>,
+    #[account(address = spl_token::ID)]
+    pub spl_token_program: AccountInfo<'info>,
+}
---
+++ src/lib.rs
@@ -0,0 +1,329 @@
+//! Anchor-compatible SDK for the Raydium AMM program.
+// #![deny(missing_docs)]
+#![deny(rustdoc::all)]
+#![allow(rustdoc::missing_doc_code_examples)]
+#![allow(clippy::nonstandard_macro_braces)]
+
+mod accounts;
+mod instructions;
+
+pub use accounts::*;
+pub use instructions::*;
+
+use anchor_lang::{prelude::*,solana_program};
+
+/// Creates a `pre_initialize` instruction
+pub fn pre_initialize<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, PreInitialize<'info>>,
+    nonce: u8,
+) -> Result<()> {
+    let ix = instructions::pre_initialize(
+        ctx.program.key,
+        ctx.accounts.amm_target_orders.key,
+        ctx.accounts.pool_withdraw_queue.key,
+        ctx.accounts.amm_authority.key,
+        ctx.accounts.lp_mint.key,
+        ctx.accounts.coin_mint.key,
+        ctx.accounts.pc_mint.key,
+        ctx.accounts.pool_coin_token_account.key,
+        ctx.accounts.pool_pc_token_account.key,
+        ctx.accounts.pool_temp_lp_token_account.key,
+        ctx.accounts.serum_market.key,
+        ctx.accounts.user_wallet.key,
+        nonce,
+    )?;
+
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.amm_target_orders,
+            ctx.accounts.pool_withdraw_queue,
+            ctx.accounts.amm_authority,
+            ctx.accounts.lp_mint,
+            ctx.accounts.coin_mint,
+            ctx.accounts.pc_mint,
+            ctx.accounts.pool_coin_token_account,
+            ctx.accounts.pool_pc_token_account,
+            ctx.accounts.pool_temp_lp_token_account,
+            ctx.accounts.serum_market,
+            ctx.accounts.user_wallet,
+            ctx.accounts.spl_token_program,
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+        ctx.signer_seeds,
+    )?;
+    Ok(())
+}
+
+/// Creates an 'initialize' instruction.
+pub fn initialize<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, Initialize<'info>>,
+    nonce: u8,
+    open_time: u64,
+) -> Result<()> {
+    let ix = instructions::initialize(
+        ctx.program.key,
+        ctx.accounts.amm.key,
+        ctx.accounts.amm_authority.key,
+        ctx.accounts.amm_open_orders.key,
+        ctx.accounts.lp_mint.key,
+        ctx.accounts.coin_mint.key,
+        ctx.accounts.pc_mint.key,
+        ctx.accounts.pool_coin_token_account.key,
+        ctx.accounts.pool_pc_token_account.key,
+        ctx.accounts.pool_withdraw_queue.key,
+        ctx.accounts.pool_target_orders_account.key,
+        ctx.accounts.pool_lp_token_account.key,
+        ctx.accounts.pool_temp_lp_token_account.key,
+        ctx.accounts.serum_program.key,
+        ctx.accounts.serum_market.key,
+        ctx.accounts.user_wallet.key,
+        nonce,
+        open_time,
+    )?;
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.amm,
+            ctx.accounts.amm_authority,
+            ctx.accounts.amm_open_orders,
+            ctx.accounts.lp_mint,
+            ctx.accounts.coin_mint,
+            ctx.accounts.pc_mint,
+            ctx.accounts.pool_coin_token_account,
+            ctx.accounts.pool_pc_token_account,
+            ctx.accounts.pool_withdraw_queue,
+            ctx.accounts.pool_target_orders_account,
+            ctx.accounts.pool_lp_token_account,
+            ctx.accounts.pool_temp_lp_token_account,
+            ctx.accounts.serum_program,
+            ctx.accounts.serum_market,
+            ctx.accounts.user_wallet,
+            ctx.accounts.spl_token_program,
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ],
+        ctx.signer_seeds,
+    )?;
+    Ok(())
+}
+
+/// Creates an 'deposit' instruction.
+pub fn deposit<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, Deposit<'info>>,
+    max_coin_amount: u64,
+    max_pc_amount: u64,
+    base_side: u64,
+) -> Result<()> {
+    let ix = instructions::deposit(
+        ctx.program.key,
+        ctx.accounts.amm.key,
+        ctx.accounts.amm_authority.key,
+        ctx.accounts.amm_open_orders.key,
+        ctx.accounts.amm_target_orders.key,
+        ctx.accounts.lp_mint.key,
+        ctx.accounts.pool_coin_token_account.key,
+        ctx.accounts.pool_pc_token_account.key,
+        ctx.accounts.serum_market.key,
+        ctx.accounts.user_coin_token_account.key,
+        ctx.accounts.user_pc_token_account.key,
+        ctx.accounts.user_lp_token_account.key,
+        ctx.accounts.user_owner.key,
+        max_coin_amount,
+        max_pc_amount,
+        base_side,
+    )?;
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.amm,
+            ctx.accounts.amm_authority,
+            ctx.accounts.amm_open_orders,
+            ctx.accounts.amm_target_orders,
+            ctx.accounts.lp_mint,
+            ctx.accounts.pool_coin_token_account,
+            ctx.accounts.pool_pc_token_account,
+            ctx.accounts.serum_market,
+            ctx.accounts.user_coin_token_account,
+            ctx.accounts.user_pc_token_account,
+            ctx.accounts.user_lp_token_account,
+            ctx.accounts.user_owner,
+            ctx.accounts.spl_token_program,
+        ],
+        ctx.signer_seeds,
+    )?;
+    Ok(())
+}
+
+/// Creates an 'withdraw' instruction.
+pub fn withdraw<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, Withdraw<'info>>,
+    amount: u64,
+) -> Result<()> {
+    let ix = instructions::withdraw(
+        ctx.program.key,
+        ctx.accounts.amm.key,
+        ctx.accounts.amm_authority.key,
+        ctx.accounts.amm_open_orders.key,
+        ctx.accounts.amm_target_orders.key,
+        ctx.accounts.lp_mint.key,
+        ctx.accounts.pool_coin_token_account.key,
+        ctx.accounts.pool_pc_token_account.key,
+        ctx.accounts.pool_withdraw_queue.key,
+        ctx.accounts.pool_temp_lp_token_account.key,
+        ctx.accounts.serum_program.key,
+        ctx.accounts.serum_market.key,
+        ctx.accounts.serum_coin_vault_account.key,
+        ctx.accounts.serum_pc_vault_account.key,
+        ctx.accounts.serum_vault_signer.key,
+        ctx.accounts.user_lp_token_account.key,
+        ctx.accounts.user_coin_token_account.key,
+        ctx.accounts.user_pc_token_account.key,
+        ctx.accounts.user_owner.key,
+        ctx.accounts.serum_event_q.key,
+        ctx.accounts.serum_bids.key,
+        ctx.accounts.serum_asks.key,
+        amount,
+    )?;
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.amm,
+            ctx.accounts.amm_authority,
+            ctx.accounts.amm_open_orders,
+            ctx.accounts.amm_target_orders,
+            ctx.accounts.lp_mint,
+            ctx.accounts.pool_coin_token_account,
+            ctx.accounts.pool_pc_token_account,
+            ctx.accounts.pool_withdraw_queue,
+            ctx.accounts.pool_temp_lp_token_account,
+            ctx.accounts.serum_program,
+            ctx.accounts.serum_market,
+            ctx.accounts.serum_coin_vault_account,
+            ctx.accounts.serum_pc_vault_account,
+            ctx.accounts.serum_vault_signer,
+            ctx.accounts.user_lp_token_account,
+            ctx.accounts.user_coin_token_account,
+            ctx.accounts.user_pc_token_account,
+            ctx.accounts.user_owner,
+            ctx.accounts.serum_event_q,
+            ctx.accounts.serum_bids,
+            ctx.accounts.serum_asks,
+            ctx.accounts.spl_token_program,
+        ],
+        ctx.signer_seeds,
+    )?;
+    Ok(())
+}
+
+/// Creates an 'swap_base_in' instruction.
+pub fn swap_base_in<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, SwapBaseIn<'info>>,
+    amount_in: u64,
+    minimum_amount_out: u64,
+) -> Result<()> {
+    let ix = instructions::swap_base_in(
+        ctx.program.key,
+        ctx.accounts.amm.key,
+        ctx.accounts.amm_authority.key,
+        ctx.accounts.amm_open_orders.key,
+        ctx.accounts.amm_target_orders.key,
+        ctx.accounts.pool_coin_token_account.key,
+        ctx.accounts.pool_pc_token_account.key,
+        ctx.accounts.serum_program.key,
+        ctx.accounts.serum_market.key,
+        ctx.accounts.serum_bids.key,
+        ctx.accounts.serum_asks.key,
+        ctx.accounts.serum_event_queue.key,
+        ctx.accounts.serum_coin_vault_account.key,
+        ctx.accounts.serum_pc_vault_account.key,
+        ctx.accounts.serum_vault_signer.key,
+        ctx.accounts.user_source_token_account.key,
+        ctx.accounts.user_destination_token_account.key,
+        ctx.accounts.user_source_owner.key,
+        amount_in,
+        minimum_amount_out,
+    )?;
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.amm,
+            ctx.accounts.amm_authority,
+            ctx.accounts.amm_open_orders,
+            ctx.accounts.amm_target_orders,
+            ctx.accounts.pool_coin_token_account,
+            ctx.accounts.pool_pc_token_account,
+            ctx.accounts.serum_program,
+            ctx.accounts.serum_market,
+            ctx.accounts.serum_bids,
+            ctx.accounts.serum_asks,
+            ctx.accounts.serum_event_queue,
+            ctx.accounts.serum_coin_vault_account,
+            ctx.accounts.serum_pc_vault_account,
+            ctx.accounts.serum_vault_signer,
+            ctx.accounts.user_source_token_account,
+            ctx.accounts.user_destination_token_account,
+            ctx.accounts.user_source_owner,
+            ctx.accounts.spl_token_program,
+        ],
+        ctx.signer_seeds,
+    )?;
+    Ok(())
+}
+
+
+/// Creates an 'swap_base_out' instruction.
+pub fn swap_base_out<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, SwapBaseOut<'info>>,
+    max_amount_in: u64,
+    amount_out: u64,
+) -> Result<()> {
+    let ix = instructions::swap_base_out(
+        ctx.program.key,
+        ctx.accounts.amm.key,
+        ctx.accounts.amm_authority.key,
+        ctx.accounts.amm_open_orders.key,
+        ctx.accounts.amm_target_orders.key,
+        ctx.accounts.pool_coin_token_account.key,
+        ctx.accounts.pool_pc_token_account.key,
+        ctx.accounts.serum_program.key,
+        ctx.accounts.serum_market.key,
+        ctx.accounts.serum_bids.key,
+        ctx.accounts.serum_asks.key,
+        ctx.accounts.serum_event_queue.key,
+        ctx.accounts.serum_coin_vault_account.key,
+        ctx.accounts.serum_pc_vault_account.key,
+        ctx.accounts.serum_vault_signer.key,
+        ctx.accounts.user_source_token_account.key,
+        ctx.accounts.user_destination_token_account.key,
+        ctx.accounts.user_source_owner.key,
+        max_amount_in,
+        amount_out,
+    )?;
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.amm,
+            ctx.accounts.amm_authority,
+            ctx.accounts.amm_open_orders,
+            ctx.accounts.amm_target_orders,
+            ctx.accounts.pool_coin_token_account,
+            ctx.accounts.pool_pc_token_account,
+            ctx.accounts.serum_program,
+            ctx.accounts.serum_market,
+            ctx.accounts.serum_bids,
+            ctx.accounts.serum_asks,
+            ctx.accounts.serum_event_queue,
+            ctx.accounts.serum_coin_vault_account,
+            ctx.accounts.serum_pc_vault_account,
+            ctx.accounts.serum_vault_signer,
+            ctx.accounts.user_source_token_account,
+            ctx.accounts.user_destination_token_account,
+            ctx.accounts.user_source_owner,
+            ctx.accounts.spl_token_program,
+        ],
+        ctx.signer_seeds,
+    )?;
+    Ok(())
+}
